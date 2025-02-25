1ba0bb1e9e3b378280b63aac6c1c465b575e3c0a
Roman Nebel <im.roman.nebel@gmail.com>
2025-02-25 14:40:18 +0100

    Added basic slides

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -12,12 +12,37 @@ type EditorProps = {
 type SlidesPreviewProps = {
   content: string
 }
+
+type SlideProps = SlidesPreviewProps & {
+  theme: string
+}
+
+function HtmlRenderer({ html }: { html: string }) {
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
+}
+
 function Editor({ value, onChange }: EditorProps) {
   return <ReactQuill theme="snow" value={value} onChange={onChange} />
 }
 
 function SlidesPreview({ content }: SlidesPreviewProps) {
-  return <div className="slides">{content}</div>
+  return (
+    <div className="previewContainer">
+      <div className="slidesList">
+        {['new', 'wave', 'neon'].map((theme) => (
+          <Slide content={content} theme={theme} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+function Slide({ content, theme }: SlideProps) {
+  return (
+    <section className={`slide _${theme}`}>
+      {HtmlRenderer({ html: content })}
+    </section>
+  )
 }
 
 export default function Main() {

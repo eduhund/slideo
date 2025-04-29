664c2ee9bba6ce0e94a3c7a22c3c91784cd9cc63
Roman Nebel <r@nebel.im>
2025-04-29 14:36:53 +0200

    Remove unused handleTextChange function and related commented-out code in Editor component: Streamline the code by eliminating unnecessary logic for header formatting based on text changes, enhancing clarity and maintainability.

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -56,15 +56,6 @@ const CustomToolbar = () => (
 function Editor({ value, onChange }: EditorProps) {
   const quillRef = useRef<ReactQuill>(null)
 
-  /*
-  useEffect(() => {
-    const quill = quillRef.current?.getEditor()
-    if (!quill) return
-
-    quill.formatLine(0, 1, 'header', 1)
-  }, [])
-  */
-
   useEffect(() => {
     if (!quillRef.current) return;
 
@@ -98,34 +89,6 @@ function Editor({ value, onChange }: EditorProps) {
       quill.format('strike', !quill.getFormat().strike);
     });
   }, [])
-  const handleTextChange = (value: any) => {
-    onChange(value)
-
-    const quill = quillRef.current?.getEditor()
-    if (!quill) return
-
-    const text = quill.getText()
-    const lines = text.split('\n')
-    let offset = 0
-    let emptyLines = 0
-
-    lines.forEach((line, index) => {
-      if (index === 0) {
-        quill.formatLine(offset, 1, 'header', 1)
-      } else if (line === '') {
-        emptyLines++
-        quill.formatLine(offset, 1, 'header', false)
-      } else {
-        if (emptyLines >= 2) {
-          quill.formatLine(offset, 1, 'header', 2)
-        } else {
-          quill.formatLine(offset, 1, 'header', false)
-        }
-        emptyLines = 0
-      }
-      offset += line.length + 1
-    })
-  }
   return (
     <div><CustomToolbar />
       <ReactQuill

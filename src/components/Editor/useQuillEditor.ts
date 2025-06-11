d219572715002c4c660211b0060bc8ea9b79a72d
Roman Nebel <r@nebel.im>
2025-06-11 14:38:57 +0200

    Enhance slide identification in Quill editor: add data-slide-id attributes and update CSS for dynamic slide numbering

--- src/components/Editor/useQuillEditor.ts
+++ src/components/Editor/useQuillEditor.ts
@@ -24,6 +24,43 @@ export default function useQuillEditor() {
   }, [state.content])
 
   useEffect(() => {
+    if (!editorRef.current) return
+
+    const root = editorRef.current.children[0]
+    if (!root) return
+    let slideId = 1
+
+    root.querySelectorAll('[data-slide-id]').forEach((el) => {
+      el.removeAttribute('data-slide-id')
+    })
+
+    root.childNodes.forEach((node, index) => {
+      if (index === 0) {
+        console.log('First fire', node, index)
+        ;(node as Element).setAttribute('data-slide-id', String(slideId))
+        slideId++
+        return
+      }
+      const prev1 = node.previousSibling || null
+      const prev2 = prev1?.previousSibling || null
+
+      const isEmpty = (el: ChildNode | null) => {
+        return el?.firstChild?.nodeName === 'BR' || !el?.textContent?.trim()
+      }
+
+      if (
+        (node as Element).nodeName === 'H1' ||
+        (node as Element).nodeName === 'H2' ||
+        (isEmpty(prev1) && isEmpty(prev2))
+      ) {
+        console.log('all fires', node, index)
+        ;(node as Element).setAttribute('data-slide-id', String(slideId))
+        slideId++
+      }
+    })
+  }, [state.content])
+
+  useEffect(() => {
     if (!editorRef.current || quillRef.current) return
 
     const quill = new Quill(editorRef.current, {

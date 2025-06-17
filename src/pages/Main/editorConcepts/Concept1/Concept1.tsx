06a704466bacd62222180bf5f6d1c96b9dcd62b1
Roman Nebel <r@nebel.im>
2025-06-17 10:53:34 +0200

    Add slide position adjustment logic in Concept1 component for improved layout

--- src/pages/Main/editorConcepts/Concept1/Concept1.tsx
+++ src/pages/Main/editorConcepts/Concept1/Concept1.tsx
@@ -4,28 +4,52 @@ import Editor from '../../../../components/Editor/Editor'
 import SlideVariants from '../../../../components/SlideVariants/SlideVariants'
 import './Concept1.scss'
 
+function getAdjustSlidePosition(index: number) {
+  const container = document.querySelector('.container')
+  const containerTop = container?.getBoundingClientRect()?.top || 0
+  const editorSlideStart = document.querySelector(
+    `[data-slide-id="${index + 1}"]`
+  )
+  const nextSlide = document.querySelector(`[data-slide-id="${index + 2}"]`)
+  const editorSlideEnd = nextSlide ? nextSlide.previousElementSibling : null
+
+  const editorSlideTop = editorSlideStart?.getBoundingClientRect().top || 0
+  const editorSlideBottom = editorSlideEnd?.getBoundingClientRect().bottom || 0
+  const editorSlideHeight = editorSlideBottom - editorSlideTop
+
+  if (editorSlideEnd && editorSlideHeight !== 0 && editorSlideHeight < 180) {
+    ;(editorSlideEnd as HTMLElement).style.marginBottom =
+      `${172 - editorSlideHeight}px`
+  }
+  return editorSlideTop - containerTop
+}
+
 export default function Concept1() {
   const { state, dispatch } = useContext(SlidesContext)
   const { slides } = state
+  console.log('Concept1', slides)
   return (
     <div className="container">
       <Editor />
       <div className="slidesVariantsContainer">
-        {slides.map((slide, index) => (
-          <SlideVariants
-            key={index}
-            slide={slide}
-            onSelect={(action: any) => {
-              if (action.type === 'UPDATE_SLIDE') {
-                dispatch({
-                  type: 'UPDATE_SLIDE',
-                  payload: { ...action.payload, index: index + 1 },
-                })
-              }
-            }}
-            style={{ top: `${index * 180}px` }}
-          />
-        ))}
+        {slides.map((slide, index) => {
+          const slideAdjust = getAdjustSlidePosition(index)
+          return (
+            <SlideVariants
+              key={index}
+              slide={slide}
+              onSelect={(action: any) => {
+                if (action.type === 'UPDATE_SLIDE') {
+                  dispatch({
+                    type: 'UPDATE_SLIDE',
+                    payload: { ...action.payload, index: index + 1 },
+                  })
+                }
+              }}
+              style={{ top: `${slideAdjust}px` }}
+            />
+          )
+        })}
       </div>
     </div>
   )

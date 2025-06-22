c39813343977a7dfccf9b7c6805cd233449517fc
Roman Nebel <r@nebel.im>
2025-06-22 23:22:07 +0200

    Add Concept3 component with layout adjustments and slide preview functionality

--- src/pages/Main/editorConcepts/Concept3/Concept3.tsx
+++ src/pages/Main/editorConcepts/Concept3/Concept3.tsx
@@ -1,20 +1,74 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { SlidesContext } from '../../../../providers'
 import Editor from '../../../../components/Editor/Editor'
-import SlidesPreview from '../../../../components/SlidesPreview/SlidesPreview'
+import SlidesPreview, {
+  SlidePreview,
+} from '../../../../components/SlidesPreview/SlidesPreview'
 import SlideVariants from '../../../../components/SlideVariants/SlideVariants'
 
 import './Concept3.scss'
+
+function getAdjustSlidePosition(index: number) {
+  const container = document.querySelector('.container')
+  const containerTop = container?.getBoundingClientRect()?.top || 0
+  const editorSlideStart = document.querySelector(
+    `[data-slide-id="${index + 1}"]`
+  )
+  const nextSlide = document.querySelector(`[data-slide-id="${index + 2}"]`)
+  const editorSlideEnd = nextSlide ? nextSlide.previousElementSibling : null
+
+  const editorSlideTop = editorSlideStart?.getBoundingClientRect().top || 0
+  const editorSlideBottom = editorSlideEnd?.getBoundingClientRect().bottom || 0
+  const editorSlideHeight = editorSlideBottom - editorSlideTop
+
+  if (editorSlideEnd && editorSlideHeight !== 0 && editorSlideHeight < 232) {
+    ;(editorSlideEnd as HTMLElement).style.marginBottom =
+      `${240 - editorSlideHeight}px`
+  }
+  return editorSlideTop - containerTop
+}
+
+function SlidePreviewItem({ slide, index, onSelect }: any) {
+  const [slideAdjust, setSlideAdjust] = useState(0)
+  useEffect(() => {
+    setSlideAdjust(getAdjustSlidePosition(index))
+  }, [index])
+  return (
+    <SlidePreview
+      key={index}
+      i={index + 1}
+      slide={slide}
+      onSelect={() => onSelect(index + 1)}
+      style={{ top: `${slideAdjust}px` }}
+    />
+  )
+}
 
 export default function Concept3() {
   const { state, dispatch } = useContext(SlidesContext)
   const { slides, activeSlide } = state
 
   const slide = activeSlide ? slides[activeSlide - 1] : null
+
+  function handleSlideSelect(index: number) {
+    dispatch({ type: 'CHANGE_ACTIVE_SLIDE', payload: index })
+  }
   return (
     <>
       <div className="container">
-        <Editor />
+        <div className="containerWrapper">
+          <Editor />
+          <div className="slidesVariantsContainer">
+            {slides.map((slide, i) => (
+              <SlidePreviewItem
+                key={i}
+                index={i}
+                slide={slide}
+                onSelect={() => handleSlideSelect(i + 1)}
+              />
+            ))}
+          </div>
+        </div>
         <SlideVariants slide={slide} onSelect={dispatch} />
       </div>
       <SlidesPreview />

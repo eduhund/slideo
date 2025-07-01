e587639105e03c568da7837c50969a8fc3fffa42
Olga Buberman <bub@sobakapav.ru>
2025-07-01 23:41:05 +0300

    Templates P0L0I2 + font size

---
+++ src/slides/Default/P0L0I2/V19/V19.tsx
@@ -0,0 +1,52 @@
+import Slide from '../../../../components/Slide/Slide'
+
+import './V19.scss'
+
+const meta = {
+  name: 'p0l0i2_v19',
+  title: {
+    level: 1,
+    minLength: 1,
+    maxLength: 100,
+  },
+  image: {
+    count: 2,
+  },
+}
+
+export function Component({ content, isSelected, onClick }: any) {
+  return (
+    <Slide
+      className={'slide titleSlide ' + meta.name}
+      data-title={meta.name}
+      isSelected={isSelected}
+      onClick={onClick}
+    >
+       <div className="slideContainer">
+          <div className="textContainer">
+             <img
+               src={content.images[1]?.src}
+               alt={content.title}
+               className="slideImage2"
+             />
+            <h1>{content.title}</h1>
+            {content.paragraphsRaw.map((p) => (<p dangerouslySetInnerHTML={{ __html: p }}/>))}
+
+            <div className="images">
+              <img
+                src={content.images[0]?.src}
+                alt={content.title}
+                className="slideImage1"
+              />
+
+            </div>
+          </div>
+        </div>
+    </Slide>
+  )
+}
+
+export default {
+  meta,
+  component: Component,
+}

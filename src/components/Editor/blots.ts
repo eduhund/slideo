2e302e5e43568710ca70a51ec8883375fc128798
Roman Nebel <r@nebel.im>
2025-07-15 12:33:49 +0200

    Add mock mermaid diagram to AIMermaidBlot class and update innerText assignment

--- src/components/Editor/blots.ts
+++ src/components/Editor/blots.ts
@@ -52,6 +52,12 @@ export class AIMermaidBlot extends BlockEmbed {
 
   static create(value: any) {
     const node = super.create(value)
+    const mockMermaid =
+      '```mermaid\ngraph TB\n    A[Запустить MVP] -->|У команды продукта есть четко прописанное техническое задание| B[Интерфейс продукта]\n    B -->|глобальная доступность| C[Глобальная доступность]\n    B -->|инновации| D[Инновации]\n    B -->|доступность| E[Доступность]\n    B -->|безопасность| F[Безопасность]\n    A -->|Выбрать команду дизайнеров с подходящими компетенциями| G[Команда дизайнеров]\n    G -->|создание интерфейсов для мобильных приложений в сфере финансов и страхования| H[Опыт в создании интерфейсов для мобильных приложений в сфере финансов и страхования]\n```'
+
+    node.className = 'ql-mermaid-diagram'
+    node.innerText = mockMermaid
+    /*
     node.setAttribute('contenteditable', false)
     node.classList.add('loading')
 
@@ -71,7 +77,7 @@ export class AIMermaidBlot extends BlockEmbed {
     generateMermaid(selectedText, '')
       .then((mermaidCode) => {
         if (!mermaidCode) throw new Error('No mermaid code in response')
-
+          
         const newNode = document.createElement('div')
         newNode.className = 'ql-mermaid-diagram'
         newNode.innerText = mermaidCode
@@ -81,6 +87,7 @@ export class AIMermaidBlot extends BlockEmbed {
         if (err.name !== 'AbortError') console.error('API Error:', err)
         if (node.parentNode) node.parentNode.removeChild(node)
       })
+      */
     return node
   }
 }
